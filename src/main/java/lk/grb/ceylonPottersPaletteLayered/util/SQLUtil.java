7b12ac9cb18988af102ba8637bc5049e00731cf9
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-12-24 21:49:58 +0530

    db, util, qr Packages Implemented

---
+++ src/main/java/lk/grb/ceylonPottersPaletteLayered/util/SQLUtil.java
@@ -0,0 +1,21 @@
+package lk.grb.ceylonPottersPaletteLayered.util;
+
+import lk.grb.ceylonPottersPalette.db.DbConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SQLUtil {
+    public static <T>T execute(String sql, Object...arg) throws SQLException {
+        PreparedStatement statement = DbConnection.getInstance().getConnection().prepareStatement(sql);
+
+        for (int i = 0; i < arg.length; i++) {
+            statement.setObject((i+1),arg[i]);
+        }
+        if (sql.startsWith("SELECT") || sql.startsWith("select")){
+            return (T) statement.executeQuery();
+        }else {
+            return(T)(Boolean)(statement.executeUpdate()>0);
+        }
+    }
+}
